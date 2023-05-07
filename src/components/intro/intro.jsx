3a328b9b91e13114d275fe196b045f3e05e1272f
snicub <danie.hangb@gmail.com>
2023-05-06 20:16:47 -0400

    fixed spelling in intro

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -9,7 +9,7 @@ const Intro = () => {
         <div className = "i">
             <div className="i-left">
                 <div className="i-left-wrapper">
-                    <h2 classname = "i-intro">Hello, my name is</h2>
+                    <h2 className = "i-intro">Hello, my name is</h2>
                     <h1 className = "i-name ">Daniel Han</h1>
                     <div className="i-title">
                         <div className="i-title-wrapper">
